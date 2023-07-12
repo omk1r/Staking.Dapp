1ca226709d1a231663e7166affa573eacc5a9910
Omkar <jadhavpatil2999@gmail.com>
2023-07-12 20:36:49 +0530

    Initial react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div classname="container">
+      <button>Buy</button>
+      <button>Stake</button>
+      <button>Unstake</button>
+      <button>Total staked</button>
+    </div>
+  );
+}
+
+export default App;
